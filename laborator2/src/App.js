1a8d08131b93b080b8f3120e92f645f1a0d6f08e
Gumaniuc Dumitru <gumaniucdima@mail.ru>
2024-05-08 12:46:36 +0300

    commit of 4 lab

--- laborator2/src/App.js
+++ laborator2/src/App.js
@@ -1,149 +1,184 @@
 import React, { useState } from 'react';
-import { Button, Form, Input, Layout, Menu } from 'antd';
+import { Button, Form, Input, Layout, Menu, Select } from 'antd';
+import { ExtendedCardInfo } from './App'; // Importăm interfața definită
 
 const { Item: MenuItem } = Menu;
 const { Header, Content, Footer } = Layout;
+const { Option } = Select;
 
 const generateRandomCardNumber = () => {
-  const generatePart = () => Math.floor(1000 + Math.random() * 9000);
-  return `${generatePart()} ${generatePart()} ${generatePart()} ${generatePart()}`;
+    const generatePart = () => Math.floor(1000 + Math.random() * 9000);
+    return `${generatePart()} ${generatePart()} ${generatePart()} ${generatePart()}`;
 };
 
 const generateRandomDateOfExpire = () => {
-  const generateMonth = () => String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
-  const generateYear = () => String(Math.floor(Math.random() * 10) + 22); // Valori între 2022 și 2031
-  return `${generateMonth()}/${generateYear()}`;
+    const generateMonth = () => String(Math.floor(Math.random() * 12) + 1).padStart(2, '0');
+    const generateYear = () => String(Math.floor(Math.random() * 10) + 22); // Valori între 2022 și 2031
+    return `${generateMonth()}/${generateYear()}`;
 };
 
 const generateRandomCVC = () => String(Math.floor(100 + Math.random() * 900)); // CVC de 3 cifre
 
 const romanianNames = ['Andrei Popescu', 'Maria Ionescu', 'Ion Vasilescu', 'Elena Dumitrescu', 'Alexandru Radulescu', 'Ana Stanciu', 'Mihai Stefanescu', 'Cristina Stan', 'George Nicolescu', 'Laura Ungureanu'];
 
-const items = new Array(3).fill(null).map((_, index) => ({
-  key: (index + 1).toString(),
-  NrCard: generateRandomCardNumber(),
-  NumberOfCard: `Card ${index + 1}`,
-  DateOfExpire: generateRandomDateOfExpire(),
-  NameOfOwner: romanianNames[Math.floor(Math.random() * romanianNames.length)],
-  CVC: generateRandomCVC(),
+const generateRandomBank = () => {
+    const banks = ['MAIB', 'Gringotts', 'MICB'];
+    return banks[Math.floor(Math.random() * banks.length)];
+};
+const generateRandomColor = () => {
+    const colors = ['blue', 'red', 'green', 'yellow'];
+    return colors[Math.floor(Math.random() * colors.length)];
+};
+
+const items: ExtendedCardInfo[] = new Array(3).fill(null).map((_, index) => ({
+    key: (index + 1).toString(),
+    NrCard: generateRandomCardNumber(),
+    NumberOfCard: `Card ${index + 1}`,
+    DateOfExpire: generateRandomDateOfExpire(),
+    NameOfOwner: romanianNames[Math.floor(Math.random() * romanianNames.length)],
+    CVC: generateRandomCVC(),
+    color: generateRandomColor(), // Generăm dinamic valoarea culorii
+    bank: generateRandomBank(),
 }));
 
 const validateCVC = (_, value) => {
-  if (!value) {
-    return Promise.reject('Vă rugăm să introduceți CVC-ul');
-  }
-  if (!/^\d{3}$/.test(value)) {
-    return Promise.reject('Vă rugăm să introduceți un CVC valid (3 cifre)');
-  }
-  return Promise.resolve();
+    if (!value) {
+        return Promise.reject('Vă rugăm să introduceți CVC-ul');
+    }
+    if (!/^\d{3}$/.test(value)) {
+        return Promise.reject('Vă rugăm să introduceți un CVC valid (3 cifre)');
+    }
+    return Promise.resolve();
 };
 
 const CustomForm = () => {
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const [formData, setFormData] = useState(null);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [formData, setFormData] = useState(null);
 
-  const onFinish = (values) => {
-    setFormSubmitted(true);
-    setFormData(values);
-    // Handle form submission logic
-    console.log('Formular trimis cu valorile:', values);
-  };
+    const onFinish = (values) => {
+        setFormSubmitted(true);
+        setFormData(values);
+        // Handle form submission logic
+        console.log('Formular trimis cu valorile:', values);
+    };
 
-  return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <Form onFinish={onFinish} style={{ width: '400px' }}>
-          <Form.Item
-              label="Număr card"
-              name="cardNumber"
-              pattern="^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$"
-              rules={[{ required: true, message: 'Vă rugăm să introduceți numărul cardului' }]}
-          >
-            <Input placeholder="Introduceți numărul cardului" />
-          </Form.Item>
-          <Form.Item
-              label="Data expirării"
-              name="expiryDate"
-              pattern="^(0[1-9]|1[0-2])\/[0-9]{2}$"
-              rules={[{ required: true, message: 'Vă rugăm să introduceți data expirării' }]}
-          >
-            <Input placeholder="Introduceți data expirării" />
-          </Form.Item>
-          <Form.Item
-              label="CVC"
-              name="cvc"
-              rules={[
-                { required: true, message: 'Vă rugăm să introduceți CVC-ul' },
-                { validator: validateCVC },
-              ]}
-          >
-            <Input placeholder="Introduceți CVC-ul" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-              Trimite
-            </Button>
-          </Form.Item>
-        </Form>
-        {formSubmitted && (
-            <div>
-              <h2>Datele formularului:</h2>
-              <p>Număr card: {formData.cardNumber}</p>
-              <p>Data expirării: {formData.expiryDate}</p>
-              <p>CVC: {formData.cvc}</p>
-            </div>
-        )}
-      </div>
-  );
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+            <Form onFinish={onFinish} style={{ width: '400px' }}>
+                <Form.Item
+                    label="Număr card"
+                    name="cardNumber"
+                    pattern="^[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}$"
+                    rules={[{ required: true, message: 'Vă rugăm să introduceți numărul cardului' }]}
+                >
+                    <Input placeholder="Introduceți numărul cardului" />
+                </Form.Item>
+                <Form.Item
+                    label="Data expirării"
+                    name="expiryDate"
+                    pattern="^(0[1-9]|1[0-2])\/[0-9]{2}$"
+                    rules={[{ required: true, message: 'Vă rugăm să introduceți data expirării' }]}
+                >
+                    <Input placeholder="Introduceți data expirării" />
+                </Form.Item>
+                <Form.Item
+                    label="CVC"
+                    name="cvc"
+                    rules={[
+                        { required: true, message: 'Vă rugăm să introduceți CVC-ul' },
+                        { validator: validateCVC },
+                    ]}
+                >
+                    <Input placeholder="Introduceți CVC-ul" />
+                </Form.Item>
+                <Form.Item
+                    label="Culoare"
+                    name="color"
+                    rules={[{ required: true, message: 'Vă rugăm să introduceți culoarea' }]}
+                >
+                    <Input placeholder="Introduceți culoarea" />
+                </Form.Item>
+                <Form.Item
+                    label="Bank"
+                    name="bank"
+                    rules={[{ required: true, message: 'Vă rugăm să introduceți banca' }]}
+                >
+                    <Select placeholder="Selectează bancă">
+                        <Option value="Example Bank 1">MAIB</Option>
+                        <Option value="Example Bank 2">Gringotts</Option>
+                        <Option value="Example Bank 3">MICB</Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                        Trimite
+                    </Button>
+                </Form.Item>
+            </Form>
+            {formSubmitted && (
+                <div>
+                    <h2>Datele formularului:</h2>
+                    <p>Număr card: {formData.cardNumber}</p>
+                    <p>Data expirării: {formData.expiryDate}</p>
+                    <p>CVC: {formData.cvc}</p>
+                    <p>Culoare: {formData.color}</p>
+                    <p>Bank: {formData.bank}</p>
+                </div>
+            )}
+        </div>
+    );
 };
 
 const App = () => {
-  const [selectedItem, setSelectedItem] = useState('1');
-  const handleMenuItemClick = (item) => setSelectedItem(item.key);
-  const selectedCard = items.find((item) => item.key === selectedItem);
+    const [selectedItem, setSelectedItem] = useState('1');
+    const handleMenuItemClick = (item) => setSelectedItem(item.key);
+    const selectedCard = items.find((item) => item.key === selectedItem);
 
-  return (
-      <Layout>
-        <Header style={{ display: 'flex', alignItems: 'center' }}>
-          <div className="demo-logo" />
-          <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['1']}
-              selectedKeys={[selectedItem]}
-              onClick={handleMenuItemClick}
-              style={{ flex: 1, minWidth: 0 }}
-          >
-            {items.map(item => (
-                <MenuItem key={item.key}>{item.NumberOfCard}</MenuItem>
-            ))}
-            <MenuItem key="form">Formular</MenuItem>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 48px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div
-              style={{
-                background: '#fff',
-                width: '60%',
-                padding: '24px',
-                borderRadius: '5px',
-                textAlign: 'center',
-              }}
-          >
-            {selectedItem === 'form' ? <CustomForm /> : (
-                <>
-                  <h2 style={{ fontSize: '24px', marginBottom: '12px' }}>Cardul {selectedCard?.NrCard}</h2>
-                  <p style={{ fontSize: '20px', marginBottom: '8px' }}>Numele proprietarului: {selectedCard?.NameOfOwner}</p>
-                  <p style={{ fontSize: '20px', marginBottom: '8px' }}>Data expirării: {selectedCard?.DateOfExpire}</p>
-                  <p style={{ fontSize: '20px', marginBottom: '8px' }}>CVC: {selectedCard?.CVC}</p>
-                </>
-            )}
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Creat de Ant UED
-        </Footer>
-      </Layout>
-  );
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', alignItems: 'center' }}>
+                <div className="demo-logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}
+                    selectedKeys={[selectedItem]}
+                    onClick={handleMenuItemClick}
+                    style={{ flex: 1, minWidth: 0 }}
+                >
+                    {items.map(item => (
+                        <MenuItem key={item.key}>{item.NumberOfCard}</MenuItem>
+                    ))}
+                    <MenuItem key="form">Formular</MenuItem>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 48px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div
+                    style={{
+                        background: '#fff',
+                        width: '60%',
+                        padding: '24px',
+                        borderRadius: '5px',
+                        textAlign: 'center',
+                    }}
+                >
+                    {selectedItem === 'form' ? <CustomForm /> : (
+                        <>
+                            <h2 style={{ fontSize: '24px', marginBottom: '12px' }}>Cardul {selectedCard?.NrCard}</h2>
+                            <p style={{ fontSize: '20px', marginBottom: '8px' }}>Numele proprietarului: {selectedCard?.NameOfOwner}</p>
+                            <p style={{ fontSize: '20px', marginBottom: '8px' }}>Data expirării: {selectedCard?.DateOfExpire}</p>
+                            <p style={{ fontSize: '20px', marginBottom: '8px' }}>CVC: {selectedCard?.CVC}</p>
+                            <p style={{ fontSize: '20px', marginBottom: '8px' }}>Culoare: {selectedCard?.color}</p>
+                            <p style={{ fontSize: '20px', marginBottom: '8px' }}>Bank: {selectedCard?.bank}</p>
+                        </>
+                    )}
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©{new Date().getFullYear()} Creat de Ant UED
+            </Footer>
+        </Layout>
+    );
 };
 
 export default App;
